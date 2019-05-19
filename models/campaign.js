b760a41625931e73c0d6e88815eb1a2ddf145865
Abdelrhman Ali <abdoali28299@gmail.com>
2019-05-19 23:48:07 +0200

    make endpoints to api

--- models/campaign.js
+++ models/campaign.js
@@ -58,3 +58,5 @@ const compaign = (campaignDB) => {
     }
 
 }
+
+module.exports = compaign
---
+++ routes/api.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const Campaign = require("../models/campaign")
+const CampaignDB = require("../models/DB/campaignDB")
+
+const router = express.Router()
+
+const campaign = new Campaign(CampaignDB)
+
+router.get('/', async (req, res) => {
+    res.json(await campaign.getCampaign())
+})
+
+router.post('/', async (req, res) => {
+    res.json(await campaign.addCampaign(req.body))
+})
