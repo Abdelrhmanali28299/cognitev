0d2d5a576ef4e060108b86e4063d2f5f74e027c5
Abdelrhman Ali <abdoali28299@gmail.com>
2019-05-19 21:55:53 +0200

    make db schema

---
+++ models/DB/campaignDB.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const campaignSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    country: {
+        type: String,
+        required: true
+    },
+    budget: {
+        type: Number,
+        required: true
+    },
+    goal:{
+        type: String,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    }
+})
+
+module.exports = mongoose.model("campaigns", campaignSchema)
