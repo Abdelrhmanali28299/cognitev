11fffb08738b59e02e17e439b0bf89b9cd7a6b91
Abdelrhman Ali <abdoali28299@gmail.com>
2019-05-23 14:47:10 +0200

    make git test unit

--- test/camaign.js
+++ test/camaign.js
@@ -1,8 +1,34 @@
-const mocha = require("mocha")
-const assert = require("assert")
+const mongoose = require('mongoose')
+const assert = require('assert')
 
-describe("Post endpoints test", () => {
-    it("get post endpoint", () => {
-        assert(1 === 1)
+const Campaign = require("../models/campaign")
+const CampaignDB = require("../models/DB/campaignDB")
+
+describe("Campaign test", () => {
+
+    beforeAll(async () => {
+        mongoose.Promise = global.Promise
+
+        await mongoose
+            .connect(global.__MONGO_URI__ + "campaign", { useNewUrlParser: true })
+            .then(() => {
+                console.log(`your database connected`)
+            })
+            .catch(err => {
+                console.log(err)
+            })
     })
+
+    afterAll(async () => {
+        await mongoose.connection.close()
+    })
+
+    it("get post endpoint", async () => {
+        const campaign = new Campaign(CampaignDB)
+        let campaigns = await campaign.getCampaign()
+        //assert(Array.isArray(campaigns) == true)
+        expect(Array.isArray(campaigns)).toEqual(true);
+    })
+
+    
 })
